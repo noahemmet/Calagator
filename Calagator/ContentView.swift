14549a0a4769407a2a34631816745509d643e785
Noah Emmet <noah@noahemmet.com>
2019-06-17 22:16:40 -0700

    Initial Commit

---
+++ Calagator/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Calagator
+//
+//  Created by Noah Emmet on 6/17/19.
+//  Copyright © 2019 Noah Emmet. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabbedView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItemLabel(Image("first"))
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItemLabel(Image("second"))
+                .tag(1)
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
