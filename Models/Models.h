d792b82005bdfc197bfef4444ec648fb7b559631
Noah Emmet <noah@noahemmet.com>
2019-06-17 23:00:37 -0700

    Models

---
+++ Models/Models.h
@@ -0,0 +1,19 @@
+//
+//  Models.h
+//  Models
+//
+//  Created by Noah Emmet on 6/17/19.
+//  Copyright © 2019 Noah Emmet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Models.
+FOUNDATION_EXPORT double ModelsVersionNumber;
+
+//! Project version string for Models.
+FOUNDATION_EXPORT const unsigned char ModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Models/PublicHeader.h>
+
+
