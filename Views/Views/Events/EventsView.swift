83672dc928cc21f1eeae2dae27a2774f5fd121ea
Noah Emmet <noah@noahemmet.com>
2019-09-12 15:48:28 -0700

    Add EventsView_Previews

--- Views/Views/Events/EventsView.swift
+++ Views/Views/Events/EventsView.swift
@@ -21,10 +21,17 @@ public struct EventsView : View {
 	}
 }
 
-//#if DEBUG
-//struct EventsView_Previews : PreviewProvider {
-//	static var previews: some View {
-//		EventsView(events: [])
-//	}
-//}
-//#endif
+struct EventsView_Previews : PreviewProvider {
+	static let start = Date()
+	static let end = start.addingTimeInterval(60*60)
+	static let tomorrowStart = Date().addingTimeInterval(60*60*24)
+	static let tomorrowEnd = tomorrowStart.addingTimeInterval(60*60)
+	static var previews: some View {
+		NavigationView {
+			EventsView(allEventsByDay: EventsByDay.from(events: [
+				.init(id: 0, title: "Event 1", summary: "Event Summary", start: start, end: end),
+				.init(id: 1, title: "Event 2", summary: "Event Summary", start: tomorrowStart, end: tomorrowEnd),
+			]))
+		}
+	}
+}
